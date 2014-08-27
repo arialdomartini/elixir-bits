61e068b6821dc07200a1a6f276c0d35678dcec6c
Arialdo Martini <arialdomartini@gmail.com>
2014-08-27 07:51:08 +0200

    Étude 2-2: A function with default values

--- geom.erl
+++ geom.erl
@@ -1,5 +1,9 @@
 -module(geom).
--export([area/2]).
+-export([area/0, area/1, area/2]).
 
 area(Length, Width) ->
     Length * Width.
+area(Length) ->
+    area(Length,1).
+area() ->
+    area(1,1).
