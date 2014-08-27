fe3752539c1c3dab4c68572aebab5aa73db125b8
Arialdo Martini <arialdomartini@gmail.com>
2014-08-27 10:04:47 +0200

    Étude 2-3: Geom module is versioned and documented

--- Geom.ex
+++ Geom.ex
@@ -1,4 +1,18 @@
 defmodule Geom do
+  @vsn 1.0
+
+  @moduledoc """
+  Geom contains functions to calculate the area of shapes.
+  This module overwrites the original Geom module, hence during the compilation warnings are produced.
+  """
+
+  @doc """
+  Calculates the area of a rectangle.
+  The default values are
+  length = 1
+  width = 1
+  """
+  @spec area(number(), number()) :: number()
   def area(length \\1, width \\1) do
     length * width
   end
