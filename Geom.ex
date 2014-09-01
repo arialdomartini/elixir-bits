0dec312fe4d437bae38d39c6470908c81f4793c5
Arialdo Martini <arialdomartini@gmail.com>
2014-09-01 18:40:46 +0200

    Étude 3-2: pattern matching with guards

--- Geom.ex
+++ Geom.ex
@@ -3,8 +3,14 @@ defmodule Geom do
 
   @spec area(atom(), number(), number()) :: number()
 
-  def area(:rectangle, length, width) do  length * width end
-  def area(:triangle, base, height) do base * height / 2 end
-  def area(:ellipse, a, b) do a * b * :math.pi end
+  def area(:rectangle, length, width) when length >= 0 and width >= 0 do
+    length * width 
+  end
+  def area(:triangle, base, height) when base >= 0 and height >= 0 do 
+    base * height / 2 
+  end
+  def area(:ellipse, a, b) when a >= 0 and b >= 0 do
+    a * b * :math.pi 
+  end
   
 end
--- geom.erl
+++ geom.erl
@@ -1,9 +1,6 @@
 -module(geom).
 -export([area/3]).
 
-area(rectangle, Length, Width) ->
-    Length * Width;
-area(ellipse, A, B) ->
-    math:pi() * A * B;
-area(triangle, Base, Height) ->
-    Base * Height / 2.
+area(rectangle, Length, Width)  when Length >= 0, Width >= 0  -> Length * Width;
+area(ellipse, A, B)             when A >= 0, B >= 0           -> math:pi() * A * B;
+area(triangle, Base, Height)    when Base >= 0, Height >= 0   -> Base * Height / 2.
