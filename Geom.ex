52da0a5d9971c36b3feeea270a13bd8855a0d627
Arialdo Martini <arialdomartini@gmail.com>
2014-09-01 09:15:57 +0200

    Using the inline syntax, as suggested by @emadb

--- Geom.ex
+++ Geom.ex
@@ -2,16 +2,9 @@ defmodule Geom do
   @vsn 1.1
 
   @spec area(atom(), number(), number()) :: number()
-  def area(:rectangle, length, width) do
-    length * width
-  end
 
-  def area(:triangle, base, height) do
-    base * height / 2
-  end
-
-  def area(:ellipse, a, b) do
-    a * b * :math.pi 
-  end
+  def area(:rectangle, length, width) do  length * width end
+  def area(:triangle, base, height) do base * height / 2 end
+  def area(:ellipse, a, b) do a * b * :math.pi end
   
 end
