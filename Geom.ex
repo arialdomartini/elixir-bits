2b7b3973974cc20a74d1b297ee2f6e0085fcedea
Arialdo Martini <arialdomartini@gmail.com>
2014-09-02 08:19:21 +0200

    Étude 4.1: use of case as an alternative to pattern matching

--- Geom.ex
+++ Geom.ex
@@ -2,21 +2,13 @@ defmodule Geom do
   @vsn 1.1
 
   @spec area(atom(), number(), number()) :: number()
-
-  def area(:rectangle, length, width) when length >= 0 and width >= 0 do
-    length * width 
-  end
-
-  def area(:triangle, base, height) when base >= 0 and height >= 0 do 
-    base * height / 2 
-  end
-
-  def area(:ellipse, a, b) when a >= 0 and b >= 0 do
-    a * b * :math.pi 
-  end
-
-  def area(_, _, _) do
-    0
+  def area(shape, a, b) do
+    case shape do
+      :rectangle -> a * b
+      :triangle  -> a * b /2
+      :ellipse   -> :math.pi * a * b
+      _          -> 0
+    end
   end
 
   @spec area( { atom(), number(), number() } ) :: number()
--- geom.erl
+++ geom.erl
@@ -4,7 +4,10 @@
 area({Shape, A, B}) ->
     area(Shape, A, B).
 
-area(rectangle, Length, Width)  when Length >= 0, Width >= 0  -> Length * Width;
-area(ellipse, A, B)             when A >= 0, B >= 0           -> math:pi() * A * B;
-area(triangle, Base, Height)    when Base >= 0, Height >= 0   -> Base * Height / 2;
-area(_, _, _) -> 0.
+area(Shape, A, B)  when A >= 0, B >= 0  ->
+    case Shape of
+        rectangle -> A * B;
+        triangle  -> A * B /2 ;
+        ellipse   -> math:pi() * A * B;
+        _         -> {error, not_implemented}
+    end.
