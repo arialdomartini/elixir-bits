05b2251e6de289446c6334fd43c89be4323fc5ec
Arialdo Martini <arialdomartini@gmail.com>
2014-09-03 08:30:02 +0200

    Excluding raise from Kernel import

--- Powers.ex
+++ Powers.ex
@@ -1,4 +1,6 @@
+import Kernel, except: [raise: 2]
+
 defmodule Powers do
-  def raisez(_, 1) do 1 end
-  def raisez(x, n) do x * raisez(x, n-1)  end
+  def raise(_, 1) do 1 end
+  def raise(x, n) do x * raise(x, n-1)  end
 end
