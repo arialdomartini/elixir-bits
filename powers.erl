71297f2cbec808c32821743324fa1cbf76094e50
Arialdo Martini <arialdomartini@gmail.com>
2014-09-03 08:35:19 +0200

    Inline code indentation

--- powers.erl
+++ powers.erl
@@ -1,9 +1,6 @@
 -module(powers).
 -export([raise/2]).
 
-raise(X, N) when N < 0 -> 
-    1/raise(X, -N);
-raise(_, 1) ->
-    1;
-raise(X, N) ->
-    X * raise(X, N-1).
+raise(X, N) when N < 0 -> 1/raise(X, -N);
+raise(_, 1)            -> 1;
+raise(X, N)            -> X * raise(X, N-1).
