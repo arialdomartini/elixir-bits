07541be258b7cf1e8fef6fd6dabfcedca0c42d94
Arialdo Martini <arialdomartini@gmail.com>
2014-09-03 08:33:28 +0200

    The Elixir version can raise numbers to negative exponents

--- Powers.ex
+++ Powers.ex
@@ -1,6 +1,7 @@
 import Kernel, except: [raise: 2]
 
 defmodule Powers do
+  def raise(x, n) when n < 0 do 1/raise(x, -n) end
   def raise(_, 1) do 1 end
   def raise(x, n) do x * raise(x, n-1)  end
 end
