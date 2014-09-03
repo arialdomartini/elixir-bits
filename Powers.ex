fed7bd975ad9686740cc9d60b2ee383f27c83779
Arialdo Martini <arialdomartini@gmail.com>
2014-09-03 08:48:35 +0200

    Étude 4-4: Tail Recursive version of Powers, in Elixir

--- Powers.ex
+++ Powers.ex
@@ -2,6 +2,9 @@ import Kernel, except: [raise: 2]
 
 defmodule Powers do
   def raise(x, n) when n < 0 do 1/raise(x, -n) end
-  def raise(_, 1) do 1 end
-  def raise(x, n) do x * raise(x, n-1)  end
+  def raise(x, n) do raise_acc(x, n, 1) end
+
+  def raise_acc(_, 1, a) do a end
+  def raise_acc(x, n, a) do raise_acc(x, n-1, x * a) end
+
 end
